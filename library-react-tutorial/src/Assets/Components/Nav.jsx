71530e7fe03a4d34bd72019f44b859151e28176a
CyberLynx1021 <abdulazizali234@gmail.com>
2023-07-29 01:19:26 -0500

    react tutorial 15min

--- library-react-tutorial/src/Assets/Components/Nav.jsx
+++ library-react-tutorial/src/Assets/Components/Nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome/'
-import LibraryLogo '../Assets/Library.svg'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome/';
+import LibraryLogo from '../Assets/Library.svg'
+
 const Nav = () => {
     return (
         <nav>
-            <div className='nav__container'></div>
+            <div className='nav__container'>
                <a href="/">
-            <img src="" alt="" className="logo" />
+            <img src={LibraryLogo} alt="" className="logo" />
             </a>
             <ul className="Nav__links">
                 <li className="nav__list">
@@ -19,10 +20,11 @@ const Nav = () => {
                     Books
                     </a> 
                     </li> 
-                    <button className='tbn__menu'>
+                    <button className='btn__menu'>
                         <FontAwesomeIcon icon='bars'/>
                     </button>
                      </ul>
+                     </div>
         </nav>
     )
 }
